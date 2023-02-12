c2dea14f97b277a38d9870d05555616308dcce6d
Jerry Kumar <jerry.kumar@trademe.co.nz>
2023-02-13 09:33:33 +1300

    minor fixes

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace DiscountCart
     {
         public static void Main(string[] args)
         {
-            var allDiscounts = new List<IDiscount>()
+            var allDiscounts = new List<IDiscount>
             {
                 new TwoForOneDiscount()
             };
